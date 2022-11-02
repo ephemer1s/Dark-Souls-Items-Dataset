c52679325600ae621dd79a12964ccdd1c18ca9cf
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-11-02 03:57:11 -0400

    Delete main.py

--- experiments/wikiReader/wikiReader.py
+++ experiments/wikiReader/wikiReader.py
@@ -1,3 +1,10 @@
+'''
+1. Read html from folder list
+2. Extract text from html
+3. Save sampled context to tsv file
+'''
+
+
 import os
 import re
 import csv
@@ -51,4 +58,17 @@ def createTitle(fname, title=None):
         writer = csv.writer(f, delimiter='\t', lineterminator='\n')
         writer.writerow(title)
     
-    
+if __name__ == "__main__":
+    # Create title for tsv file
+    tsv = 'data/experiment.tsv'
+    createTitle(tsv)
+        
+    # traverse all files and save samples
+    for fname in os.listdir('./raw/'):
+        print(fname)
+        if os.path.isfile(os.path.join('./raw/', fname)):
+            html = readHtml(os.path.join('./raw/', fname))
+            sample = extractText(html)
+            saveText(tsv, sample)
+        else:
+            print(fname, 'is not a file')
